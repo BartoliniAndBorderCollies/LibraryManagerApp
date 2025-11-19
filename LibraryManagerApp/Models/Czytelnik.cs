61aad87bc83bc047347eb87e81133bb49cd22560
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2025-11-19 21:35:50 +0100

    Dodaj pliki projektów.

---
+++ LibraryManagerApp/Models/Czytelnik.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LibraryManagerApp.Models
+{
+    public class Czytelnik
+    {
+        [Key]
+        public int IdCzytelnika { get; set; }
+
+        [Required]
+        public string Imie { get; set; }
+
+        [Required]
+        public string Nazwisko { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        public string? Telefon { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime DataRejestracji { get; set; }
+
+        public bool Aktywny { get; set; }
+    }
+}

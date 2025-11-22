ab3185f44a3555d7d74cc19085090163f9aa2062
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2025-11-22 17:07:10 +0100

    dodanie generycznego interfejsu IRepository

---
+++ LibraryManagerApp/Repositories/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace LibraryManagerApp.Repositories
+{
+    public interface IRepository<T> where T : class
+    {
+        Task<IEnumerable<T>> GetAllAsync(); //tylko po odczycie wiec lepiej interfejs Enumerable
+        Task<T> GetByIdAsync(int id);
+        Task<T> AddAsync(T entity);
+        Task<T> UpdateAsync(int id, T entity);
+        Task DeleteAsync(T entity);
+        Task DeleteAllAsync();
+    }
+
+}

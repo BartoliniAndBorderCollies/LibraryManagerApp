bfe33679fecfe19bd4c47ffc55ed4cd56687e6a6
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2025-11-23 11:59:02 +0100

    dodanie mozliwosci zwracania nulli w IRepository

--- LibraryManagerApp/Repositories/IRepository.cs
+++ LibraryManagerApp/Repositories/IRepository.cs
@@ -5,9 +5,9 @@ namespace LibraryManagerApp.Repositories
     public interface IRepository<T> where T : class
     {
         Task<IEnumerable<T>> GetAllAsync(); //tylko po odczycie wiec lepiej interfejs Enumerable
-        Task<T> GetByIdAsync(int id);
+        Task<T?> GetByIdAsync(int id);
         Task<T> AddAsync(T entity);
-        Task<T> UpdateAsync(int id, T entity);
+        Task<T?> UpdateAsync(int id, T entity);
         Task DeleteAsync(T entity);
         Task DeleteAllAsync();
     }

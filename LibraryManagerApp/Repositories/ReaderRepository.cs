0aa801519d1a5448f33a5011f1826cfd9e96c265
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2025-12-03 18:29:11 +0100

    dodanie ReaderRepo wraz z implementacją metod

---
+++ LibraryManagerApp/Repositories/ReaderRepository.cs
@@ -0,0 +1,63 @@
+﻿using LibraryManagerApp.Models;
+using LibraryManagerApp.Models.EFCoreMapping;
+using Microsoft.EntityFrameworkCore;
+
+namespace LibraryManagerApp.Repositories
+{
+    public class ReaderRepository : IRepository<Reader>
+    {
+
+        public readonly LibraryContext _libraryContext;
+
+        public ReaderRepository(LibraryContext libraryContext)
+        {
+            _libraryContext = libraryContext;
+        }
+
+
+        public async Task<Reader> AddAsync(Reader entity)
+        {
+            await _libraryContext.Readers.AddAsync(entity);
+            await _libraryContext.SaveChangesAsync();
+
+            return entity;
+        }
+
+        public async Task DeleteAllAsync()
+        {
+            _libraryContext.Readers.RemoveRange(_libraryContext.Readers);
+            await _libraryContext.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(Reader entity)
+        {
+            _libraryContext.Readers.Remove(entity);
+            await _libraryContext.SaveChangesAsync();
+        }
+
+        public async Task<IEnumerable<Reader>> GetAllAsync()
+        {
+           return await _libraryContext.Readers.ToListAsync();
+        }
+
+        public async Task<Reader?> GetByIdAsync(int id)
+        {
+            return await _libraryContext.Readers.FindAsync(id);
+        }
+
+        public async Task<Reader?> UpdateAsync(int id, Reader entity)
+        {
+            Reader? reader = await _libraryContext.Readers.FindAsync(id);
+
+            if (reader == null)
+            {
+                return null;
+            }
+
+            _libraryContext.Entry(reader).CurrentValues.SetValues(entity);
+            await _libraryContext.SaveChangesAsync();
+
+            return reader;
+        }
+    }
+}

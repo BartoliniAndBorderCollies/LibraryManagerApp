50e8fde7606961b6fd67594885079d6440dc9896
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2025-12-05 12:39:52 +0100

    dodanie generycznego IService

---
+++ LibraryManagerApp/Service/IService.cs
@@ -0,0 +1,12 @@
+﻿namespace LibraryManagerApp.Service
+{
+    public interface IService<T> where T : class
+    {
+        Task<IEnumerable<T>> GetAllAsync(); //tylko po odczycie wiec lepiej interfejs Enumerable
+        Task<T?> GetByIdAsync(int id);
+        Task<T> AddAsync(T entity);
+        Task<T?> UpdateAsync(int id, T entity);
+        Task DeleteAsync(T entity);
+        Task DeleteAllAsync();
+    }
+}

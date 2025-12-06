a8aea0d62103b05cb1a881a7daa5f70ba4cb5c18
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2025-12-06 16:22:13 +0100

    dodanie AuthorController

---
+++ LibraryManagerApp/Controllers/AuthorController.cs
@@ -0,0 +1,93 @@
+﻿using LibraryManagerApp.Models;
+using LibraryManagerApp.Service;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LibraryManagerApp.Controllers
+{
+    public class AuthorController : Controller
+    {
+        public readonly AuthorService AuthorService;
+
+        public AuthorController(AuthorService authorService)
+        {
+            this.AuthorService = authorService;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> Add(Author entity)
+        {
+            await AuthorService.AddAsync(entity);
+            return RedirectToAction("Index");
+
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> DeleteAll()
+        {
+            await AuthorService.DeleteAllAsync();
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(Author entity)
+        {
+            await AuthorService.DeleteAsync(entity);
+            return RedirectToAction("Index");
+
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var authors = await AuthorService.GetAllAsync();
+            return View("Index", authors);
+
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetById(int id)
+        {
+
+            try
+            {
+                Author? author = await AuthorService.GetByIdAsync(id);
+                return View("Details", author);
+
+            }
+            catch (KeyNotFoundException ex)
+            {
+                TempData["Error"] = ex.Message;
+                return RedirectToAction("Index");
+            }
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Update(int id, Author entity)
+        {
+
+            try
+            {
+                Author? updatedAuthor = await AuthorService.UpdateAsync(id, entity);
+                return View("Details", updatedAuthor);
+
+            }
+            catch (KeyNotFoundException ex)
+            {
+                TempData["Error"] = ex.Message;
+                return RedirectToAction("Index");
+            }
+            catch (InvalidOperationException ex)
+            {
+                TempData["Error"] = ex.Message;
+                return RedirectToAction("Index");
+            }
+        }
+    }
+}

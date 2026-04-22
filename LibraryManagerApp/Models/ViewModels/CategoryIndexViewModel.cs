694278e2d2618264f6d0a3d2b0733332482dda24
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2026-04-22 10:39:53 +0200

    dodanie modelu CategoryIndexViewModel

---
+++ LibraryManagerApp/Models/ViewModels/CategoryIndexViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace LibraryManagerApp.Models.ViewModels
+{
+    public class CategoryIndexViewModel
+    {
+
+        public List<Category> Categories { get; set; } = new List<Category>();
+        public Category NewCategory { get; set; } = new Category();
+
+    }
+}

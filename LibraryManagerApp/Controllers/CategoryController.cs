745d6d7b054813d9880edfab7333660944a53710
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2026-04-22 10:42:51 +0200

    dodanie CategoryController

---
+++ LibraryManagerApp/Controllers/CategoryController.cs
@@ -0,0 +1,43 @@
+﻿using LibraryManagerApp.Models;
+using LibraryManagerApp.Models.ViewModels;
+using LibraryManagerApp.Service;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LibraryManagerApp.Controllers
+{
+    public class CategoryController : Controller
+    {
+
+        private readonly CategoryService _categoryService;
+
+        public CategoryController(CategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Add(Category category)
+        {
+
+            await _categoryService.AddAsync(category);
+
+            return RedirectToAction("Index");
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+
+            var categories = await _categoryService.GetAllAsync();
+
+            CategoryIndexViewModel viewModel = new CategoryIndexViewModel
+            {
+                Categories = categories.ToList()
+            };
+
+            return View(viewModel);
+        }
+    }
+}

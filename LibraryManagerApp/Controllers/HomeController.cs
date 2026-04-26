c4c2213b76953c44ff669daa2f04002e3943e551
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2026-04-26 10:13:43 +0200

    dodanie About

--- LibraryManagerApp/Controllers/HomeController.cs
+++ LibraryManagerApp/Controllers/HomeController.cs
@@ -23,6 +23,12 @@ namespace LibraryManagerApp.Controllers
             return View();
         }
 
+        public IActionResult About()
+        {
+            return View();
+        }
+
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {

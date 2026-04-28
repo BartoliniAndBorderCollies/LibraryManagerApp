dfb132a5c3f71d086b00f8f7e88905db8792aa0d
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2026-04-28 19:30:48 +0200

    dodanie ReaderIndexViewModel

---
+++ LibraryManagerApp/Models/ViewModels/ReaderIndexViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace LibraryManagerApp.Models.ViewModels
+{
+    public class ReaderIndexViewModel
+    {
+        public List<Reader> Readers { get; set; } = new List<Reader>();
+        public Reader NewReader { get; set; } = new Reader();
+
+    }
+}

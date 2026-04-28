682759a35ded8fddbe60026c03e7118abc59be0b
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2026-04-28 19:31:02 +0200

    dodanie ReaderController z metodą Index

---
+++ LibraryManagerApp/Controllers/ReaderController.cs
@@ -0,0 +1,31 @@
+﻿using LibraryManagerApp.Models;
+using LibraryManagerApp.Models.ViewModels;
+using LibraryManagerApp.Service;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LibraryManagerApp.Controllers
+{
+    public class ReaderController : Controller
+    {
+
+        private readonly ReaderService _readerService;
+
+        public ReaderController(ReaderService readerService)
+        {
+            _readerService = readerService;
+        }
+
+
+        public async Task<IActionResult> Index()
+        {
+            var readers = await _readerService.GetAllAsync();
+
+            ReaderIndexViewModel viewModel = new ReaderIndexViewModel
+            {
+                Readers = readers.ToList()
+            };
+
+            return View(viewModel);
+        }
+    }
+}
